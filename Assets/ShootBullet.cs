6e64fac3f8c64f76088a1acb4c1bc55243edd037
Croquetox <marcosdario.granda@opendeusto.es>
2025-04-09 08:07:26 +0200

    Añadidas balas disparadas

---
+++ Assets/ShootBullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    public int force;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoShoot();
+        }
+    }
+
+    void DoShoot()
+    {
+        GameObject instanciatedBullet = Instantiate(bullet, this.transform.position, this.transform.rotation);
+        instanciatedBullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+    }
+}
